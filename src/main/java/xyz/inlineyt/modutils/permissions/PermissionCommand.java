0a9947e2a3e1c107eb27d7471ccf07981409bd08
Inlineyt <80773746+Inlineyt@users.noreply.github.com>
2022-02-13 10:43:07 +0100

    delete things in permissions which not been used by me

--- src/main/java/xyz/inlineyt/modutils/permissions/PermissionCommand.java
+++ src/main/java/xyz/inlineyt/modutils/permissions/PermissionCommand.java
@@ -9,30 +9,11 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class PermissionCommand implements CommandExecutor {
+    
+    
     @Override
+    public boolean onCommand( CommandSender sender, Command command,  String label, String[] args) {
 
-
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-  //         if (Bukkit.getPlayer(args[0]) == null) {
-         //       sender.sendMessage("Dieser Spieler ist nicht Online");
-    //        }
-    //        if(args[1].equalsIgnoreCase("add")) {
-//
- //               Player player = Bukkit.getPlayer(args[0]);
-   //             CloudNetDriver.getInstance().getPermissionManagement().modifyUser(player.getUniqueId(), permissionUser -> {
-     //               permissionUser.addGroup(args[2]);
-
-       //             sender.sendMessage("der Spieler" + player + "ist nun in der :" + args[2] + "Gruppe");
-
-         //       });
-           // }
-            //else if(args[1].equalsIgnoreCase("remove")) {
-            //    Player player = Bukkit.getPlayer(args[0]);
-            //    CloudNetDriver.getInstance().getPermissionManagement().modifyUser(player.getUniqueId(), permissionUser -> {
-             //       permissionUser.removeGroup(args[2]);
-             //       sender.sendMessage("der Spieler" + player + "ist nun nicht mehr in der :" + args[2] + "Gruppe");
-              //  });
-        //    }
         return false;
     }
 
